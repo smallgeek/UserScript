1c367faaae16477704a35e2a19c6e21c32666307
smallgeek <smallgeek@outlook.jp>
2020-08-23 19:54:15 +0900

    Pixiv の作品ページの評価を非表示の対象に追加

--- hide-praise-on-pixiv.js
+++ hide-praise-on-pixiv.js
@@ -14,11 +14,12 @@ function hideElements() {
      // いいね、コメント、閲覧数の要素を取得
     const reports = Array.from(document.querySelectorAll('.display-report'));
     const lists = Array.from(document.querySelectorAll('.count-list'));
+    const artworks = Array.from(document.querySelectorAll('.sc-1qvk3ka-0.dHOEJf'));
 
     // フォロワー数の要素を取得
     const follow = Array.from(document.querySelectorAll('.sc-1dlbdn6-10.dOlYed'));
 
-    for (const element of reports.concat(lists).concat(follow)) {
+    for (const element of reports.concat(lists).concat(artworks).concat(follow)) {
         element.parentElement.removeChild(element);
     }
 }
