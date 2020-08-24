10fe519bd284498ebaad987219e087135b055c83
smallgeek <smallgeek@outlook.jp>
2020-08-24 20:13:33 +0900

    Twitter の RT いいねの非表示要素を修正

--- hide-praise-on-twitter.js
+++ hide-praise-on-twitter.js
@@ -14,9 +14,10 @@ function hideElements() {
     // フォロー・フォロワー数
     const ff = Array.from(document.querySelectorAll('.css-1dbjc4n.r-18u37iz.r-1w6e6rj'));
     // リツイート・いいねの数
-    const count = Array.from(document.querySelectorAll('.css-1dbjc4n.r-xoduu5.r-1udh08x'));
+    const retweets = Array.from(document.querySelectorAll('[data-testid="retweet"]'));
+    const likes = Array.from(document.querySelectorAll('[data-testid="like"]'));
 
-    for (const element of ff.concat(count)) {
+    for (const element of ff.concat(retweets).concat(likes)) {
         element.parentElement.removeChild(element);
     }
 }
