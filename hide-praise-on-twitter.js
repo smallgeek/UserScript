a6943ee8f9cc7bad4f37da5114fb4de036462fc9
smallgeek <smallgeek@outlook.jp>
2020-08-29 14:11:40 +0900

    Twitter で自分が RT いいねした場合に数が表示されていた不具合を修正

--- hide-praise-on-twitter.js
+++ hide-praise-on-twitter.js
@@ -15,8 +15,8 @@ function hideElements() {
     const ff = Array.from(document.querySelectorAll('a.r-hkyrab.r-1loqt21.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-qvutc0.css-4rbku5.css-18t94o4.css-901oao'));
     // コメント・リツイート・いいねの数
     const replys = Array.from(document.querySelectorAll('[data-testid="reply"]')).map(el => el.children[0].children[1]);
-    const retweets = Array.from(document.querySelectorAll('[data-testid="retweet"]')).map(el => el.children[0].children[1]);
-    const likes = Array.from(document.querySelectorAll('[data-testid="like"]')).map(el => el.children[0].children[1]);;
+    const retweets = Array.from(document.querySelectorAll('[data-testid="retweet"], [data-testid="unretweet"]')).map(el => el.children[0].children[1]);
+    const likes = Array.from(document.querySelectorAll('[data-testid="like"], [data-testid="unlike"]')).map(el => el.children[0].children[1]);;
 
     for (const element of ff.concat(replys).concat(retweets).concat(likes)) {
         if (element) {
